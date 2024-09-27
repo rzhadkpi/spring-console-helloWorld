6571c2c8929a5d5d9d33e6f3816cd289a9ef3e10
rzhadkpi <zhadko.ruslan@lll.kpi.ua>
2024-09-27 21:49:27 +0300

    Add 6th task

--- src/main/java/com/example/myspringconsolehelloworld/MySpringConsoleHelloWorldApplication.java
+++ src/main/java/com/example/myspringconsolehelloworld/MySpringConsoleHelloWorldApplication.java
@@ -3,9 +3,11 @@ package com.example.myspringconsolehelloworld;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class MySpringConsoleHelloWorldApplication implements CommandLineRunner {
+public class MySpringConsoleHelloWorldApplication {
 
     public static void main(String[] args) {
         System.out.println("Begin of main!");
@@ -13,8 +15,30 @@ public class MySpringConsoleHelloWorldApplication implements CommandLineRunner {
         System.out.println("End of main!");
     }
 
-    public void run(String... args) throws Exception {
-        System.out.println("Hello from Spring Boot!");
+    @Component
+    @Order(1)
+    public static class FirstRunner implements CommandLineRunner {
+        @Override
+        public void run(String... args) throws Exception {
+            System.out.println("First");
+        }
     }
 
+    @Component
+    @Order(2)
+    public static class HelloRunner implements CommandLineRunner {
+        @Override
+        public void run(String... args) throws Exception {
+            System.out.println("Hello from Spring Boot!");
+        }
+    }
+
+    @Component
+    @Order(3)
+    public static class SecondRunner implements CommandLineRunner {
+        @Override
+        public void run(String... args) throws Exception {
+            System.out.println("Second");
+        }
+    }
 }
